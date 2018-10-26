e7fbf68d84ae776921bc33b65c217ff4b43f4073
Tom Chartrand <tmchartrand@gmail.com>
2018-10-26 12:15:43 -0700

    l5pc benchmark plotting

--- examples/l5pc/benchmark/task_stats.py
+++ examples/l5pc/benchmark/task_stats.py
@@ -1,7 +1,11 @@
 import sqlite3
 import collections
 from datetime import datetime, timedelta
-
+import matplotlib as mpl
+import os
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
